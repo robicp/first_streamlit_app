d20ae27e1517d88aa12a2264a028142237fab19f
Robert Hlevnjak <85356963+robicp@users.noreply.github.com>
2022-12-14 11:19:02 +0100

    Update streamlit_app.py
    reading a txt file for food list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 
 import streamlit
+import pandas
 
 streamlit.title ("Robert's obroci")
 
@@ -9,3 +10,6 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥗 Hard-Boiled Free-Range Egg')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+
+pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
