d83d3604c151b5ffcaca71465bde2412d134702e
Robert Hlevnjak <85356963+robicp@users.noreply.github.com>
2022-12-14 11:11:09 +0100

    Update streamlit_app.py
    add večera menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 
 import streamlit
 
-streamlit.title ("Robert's streamlit title")
+streamlit.title ("Robert's doručak večera")
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
