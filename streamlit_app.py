a8ba7e45692be52fc06580832abbaf5bb18b9b6d
Robert Hlevnjak <85356963+robicp@users.noreply.github.com>
2022-12-14 11:13:48 +0100

    Update streamlit_app.py
    adding pictures

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,11 @@
 
 import streamlit
 
-streamlit.title ("Robert's doručak večera")
+streamlit.title ("Robert's obroci")
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('🥑 Breakfast Menu')
+streamlit.text('🐔🍞 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥗 Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
