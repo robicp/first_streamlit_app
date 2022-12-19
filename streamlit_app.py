170f27334574fae60eca718c12686c83886dbd7a
Robert Hlevnjak <85356963+robicp@users.noreply.github.com>
2022-12-19 11:20:59 +0100

    Update streamlit_app.py
    delete text api

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,6 @@ streamlit.dataframe(fruits_to_show)
 #New section to display  fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
-
 # put a json in a variable
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # display as table 
