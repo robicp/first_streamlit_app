69541a17e19c6037f2aa0779ddcd05742984c54f
Robert Hlevnjak <85356963+robicp@users.noreply.github.com>
2022-12-20 10:06:38 +0100

    Update streamlit_app.py
    inserting into table fruit_load_list

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,3 +44,6 @@ streamlit.dataframe(my_data_rows)
 #adding second text entry box
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
 streamlit.write('Tnx for adding ', add_my_fruit)
+
+#inserting into table fruit_load_list
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
